b312f0086df5ad876c90785752a3db2be801e3b6
Liam <liam.ohanlon@mozaic.net>
2019-05-10 16:53:24 +0100

    Overhaul

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-#TODO: optimise
 import time, sys, warnings
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from random import randint
-
 # Supress PhantomJS deprecation warnings
 warnings.filterwarnings("ignore")
-# Hardcoded value for when Selenium can't find webdriver in PATH
 phantom_driver = "C:\\Program Files (x86)\\phantomjs\\bin\\phantomjs.exe"
 driver = webdriver.PhantomJS(phantom_driver)
 # Selenium script target URL
@@ -15,11 +12,7 @@ if len(sys.argv) > 1:
 else:
 	# Default target URL
 	target_site = "https://calculator.demo.mozaicdemo.cloud"
-print('\n Target ==> %s\n' % target_site)
-
-# Exits without error status
-def exit():
-	sys.exit(0)
+print('\n\n Target ==> %s\n' % target_site)
 
 # Convert symbols into equivalent HTML IDs
 def convert(number):
@@ -37,136 +30,79 @@ def convert(number):
 	elif number == '=':
 		result = "btnEquals"
 	else:
-		print("No conversion occured!") 
+		print("[ERROR] Conversion failed!")
+		sys.exit(1)
 	return result
 
-'''
-Tests addition functionality by generating two random integers
-between 0-9, calculating the sum internally and comparing it to
-the resulting output from the JS calculator.
-'''
-def additionTest():
-    # RNG & Internal calculations for comparison
+# Test everything
+def test():
+	error_count = 0
 	n1_int = randint(1,9)
 	n2_int = randint(1,9)
-	n1 = convert(n1_int)
-	n2 = convert(n2_int)
 	correct_answer = n1_int + n2_int
-	print('\nExpecting %r + %r = %r' % (n1_int,n2_int,correct_answer))
-	# Test calculator against internal results
+	print('---- Running Test [1/4] ----')
+	print('Expecting %r + %r = %r' % (n1_int,n2_int,correct_answer))
 	driver.get(target_site)
-	driver.maximize_window()
-	time.sleep(3)
-	driver.find_element_by_id(n1).click()
+	driver.find_element_by_id(convert(n1_int)).click()
 	driver.find_element_by_id(convert('+')).click()
-	driver.find_element_by_id(n2).click()
+	driver.find_element_by_id(convert(n2_int)).click()
 	driver.find_element_by_id(convert("=")).click()
 	ans = driver.find_element_by_class_name("log").text
 	try:
 		assert ans == str(correct_answer)
-		print('Results %r + %r = %s\n\nAddition functionality working!' % (n1_int,n2_int,ans))
-		main_menu()
+		print('Results %r + %r = %s\nAddition functionality working!' % (n1_int,n2_int,ans))
 	except AssertionError:
-		print("Results %r + %r = %s\n\n\t[!] Assertion Failed [!]\n     Addition functionality broken..." % (n1_int,n2_int,ans))
-		sys.exit(1)	
-	
-def subtractionTest():
-    # RNG & Internal calculations for comparison
-	n1_int = randint(1,9)
-	n2_int = randint(1,9)
-	n1 = convert(n1_int)
-	n2 = convert(n2_int)
+		print("Results %r + %r = %s\n\n\t[!] Assertion Failed [!]\n     Test [1/4] failed, addition functionality broken." % (n1_int,n2_int,ans))
+		error_count+=1
 	correct_answer = n1_int - n2_int
-	print('\nExpecting %r - %r = %r' % (n1_int,n2_int,correct_answer))
-	# Test calculator against internal results
+	print('\n---- Running Test [2/4] ----')
+	print('Expecting %r - %r = %r' % (n1_int,n2_int,correct_answer))
 	driver.get(target_site)
-	driver.maximize_window()
-	time.sleep(3)
-	driver.find_element_by_id(n1).click()
+	driver.find_element_by_id(convert(n1_int)).click()
 	driver.find_element_by_id(convert('-')).click()
-	driver.find_element_by_id(n2).click()
+	driver.find_element_by_id(convert(n2_int)).click()
 	driver.find_element_by_id(convert("=")).click()
 	ans = driver.find_element_by_class_name("log").text
 	try:
 		assert ans == str(correct_answer)
-		print('Results %r - %r = %s\n\nSubtraction functionality working!' % (n1_int,n2_int,ans))
-		main_menu()
+		print('Results %r - %r = %s\nSubtraction functionality working!' % (n1_int,n2_int,ans))
 	except AssertionError:
-		print("Results %r - %r = %s\n\n\t[!] Assertion Failed [!]\n     Subtraction functionality broken..." % (n1_int,n2_int,ans))
-		sys.exit(1)	
-		
-def multiplicationTest():
-    # RNG & Internal calculations for comparison
-	n1_int = randint(1,9)
-	n2_int = randint(1,9)
-	n1 = convert(n1_int)
-	n2 = convert(n2_int)
+		print("Results %r - %r = %s\n\n\t[!] Assertion Failed [!]\n     Test [2/4 failed, subtraction functionality broken." % (n1_int,n2_int,ans))
+		error_count+=1
 	correct_answer = n1_int * n2_int
-	print('\nExpecting %r x %r = %r' % (n1_int,n2_int,correct_answer))
-	# Test calculator against internal results
+	print('\n---- Running Test [3/4] ----')
+	print('Expecting %r * %r = %r' % (n1_int,n2_int,correct_answer))
 	driver.get(target_site)
-	driver.maximize_window()
-	time.sleep(3)
-	driver.find_element_by_id(n1).click()
+	driver.find_element_by_id(convert(n1_int)).click()
 	driver.find_element_by_id(convert('*')).click()
-	driver.find_element_by_id(n2).click()
+	driver.find_element_by_id(convert(n2_int)).click()
 	driver.find_element_by_id(convert("=")).click()
 	ans = driver.find_element_by_class_name("log").text
 	try:
 		assert ans == str(correct_answer)
-		print('Results %r x %r = %s\n\nMultiplication functionality working!' % (n1_int,n2_int,ans))
-		main_menu()
+		print('Results %r x %r = %s\nMultiplication functionality working!' % (n1_int,n2_int,ans))
 	except AssertionError:
-		print("Results %r x %r = %s\n\n\t[!] Assertion Failed [!]\n     Multiplication functionality broken..." % (n1_int,n2_int,ans))
-		sys.exit(1)
-	
-def divisionTest():
-    # RNG & Internal calculations for comparison
-	n1_int = randint(1,9)
-	n2_int = randint(1,9)
-	n1 = convert(n1_int)
-	n2 = convert(n2_int)
-	correct_answer = n1_int / n2_int
-	correct_answer = str(correct_answer)[:4] # Limit to 2 d.p.
-	print('\nExpecting %r / %r = %s' % (n1_int,n2_int,correct_answer))
-	# Test calculator against internal results
+		print("Results %r x %r = %s\n\n\t[!] Assertion Failed [!]\n     Test [3/4] failed, multiplication functionality broken..." % (n1_int,n2_int,ans))
+		error_count+=1
+	correct_answer = str(n1_int / n2_int)[:1]
+	print('\n---- Running Test [3/4] ----')
+	print('Expecting %r / %r = %s' % (n1_int,n2_int,correct_answer))
 	driver.get(target_site)
-	driver.maximize_window()
-	time.sleep(3)
-	driver.find_element_by_id(n1).click()
+	driver.find_element_by_id(convert(n1_int)).click()
 	driver.find_element_by_id(convert('/')).click()
-	driver.find_element_by_id(n2).click()
+	driver.find_element_by_id(convert(n2_int)).click()
 	driver.find_element_by_id(convert("=")).click()
 	ans = driver.find_element_by_class_name("log").text
+	ans = ans[:1]
 	try:
 		assert ans == str(correct_answer)
-		print('Results %r / %r = %s\n\nDivision functionality working!' % (n1_int,n2_int,ans))
-		main_menu()
+		print('Results %r / %r = %s\nDivision functionality working!' % (n1_int,n2_int,ans))
 	except AssertionError:
-		print("Results %r / %r = %s\n\n\t[!] Assertion Failed [!]\n     Division functionality broken..." % (n1_int,n2_int,ans))
+		print("Results %r / %r = %s\n\n\t\t[!] Assertion Failed [!]\n     Test [4/4] failed, division functionality broken..." % (n1_int,n2_int,ans))
+		error_count+=1
+	if error_count > 0:
 		sys.exit(1)
-	
-# Test addition functionality
-def main_menu():
-	print('''
-\t ======== MAIN MENU ========
-\t| 0) Test Addition          |
-\t| 1) Test Subtraction       |
-\t| 2) Test Multiplication    |
-\t| 3) Test Division          |
-\t|  ______________________   |
-\t| 4) Exit                   |
-\t ---------------------------
-''')
-	choice = int(input('\t  Enter your choice: '))
-	options = {
-		0: additionTest,
-		1: subtractionTest,
-		2: multiplicationTest,
-		3: divisionTest,
-		4: exit
-	}
-	options[choice]()
+	else:
+		sys.exit(0)
 
-#Entry point
-main_menu()
+test()
